c71e4d49cc13c55acef978129431ce16c29da58a
Karan Sangoi <karan@stryker-digital.com>
2025-07-14 15:31:54 +0530

    Updated home page and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import icon from "../public/hero.svg";
 import femaleCleaner from "../public/female-cleaner.svg";
@@ -21,7 +23,7 @@ import ServiceCard from "@/components/service-card";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className="px-16 py-10 lg:py-20 w-full flex flex-col lg:flex-row items-center">
+      <div className="px-16 md:px-0 py-10 lg:py-20 w-full flex flex-col lg:flex-row items-center md:max-w-[80rem]">
         <div className="flex flex-col items-center lg:items-start text-center lg:text-start space-y-4">
           <h1 className="text-4xl lg:text-6xl text-primary font-black">
             #1 House Cleaning Services in Scottsdale, AZ
@@ -40,74 +42,79 @@ export default function Home() {
         </div>
         <Image src={icon} height="500" width="500" alt="hello" />
       </div>
-      <div className="px-16 py-12 w-full flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 bg-secondary text-white">
-        <CommitmentCard
-          title="100% Guarantee"
-          description="We stand by our work with a 100% guarantee, ensuring that every clean meets your highest expectations."
-        >
-          <IconCheck className="size-16" />
-        </CommitmentCard>
-        <CommitmentCard
-          title="Best In Class Service"
-          description="Our commitment to best-in-class service means we deliver meticulous, top-tier cleaning with a personal touch."
-        >
-          <IconRibbon className="size-16" />
-        </CommitmentCard>
-        <CommitmentCard
-          title="Competitive Pricing"
-          description="Competitive pricing that ensures you receive outstanding value and quality, making a pristine home accessible to everyone."
-        >
-          <IconCheck className="size-16" />
-        </CommitmentCard>
-      </div>
-      <div className="px-16 py-10 lg:py-20 w-full flex flex-col lg:flex-row items-center bg-white">
-        <div className="flex flex-col items-center lg:items-start text-center lg:text-start space-y-4">
-          <h2 className="text-3xl lg:text-5xl text-black font-bold">
-            Commited to quality and customer satisfaction
-          </h2>
-          <p className="text-sm lg:text-lg text-slate-400">
-            We provide a range of services to meet each of your house cleaning
-            needs. Whatever problem you&apos;re facing, we have a solution!
-          </p>
-          <div className="flex flex-col items-start space-y-4">
-            <div className="flex items-center">
-              <IconCircleCheck className="size-10 mr-2 text-secondary" />
-              <p className="text-lg text-black font-semibold">
-                100% Satisfaction Guarantee
-              </p>
-            </div>
-            <div className="flex items-center">
-              <IconCircleCheck className="size-10 mr-2 text-secondary" />
-              <p className="text-lg text-black font-semibold">
-                Highly professional staff
-              </p>
-            </div>
-            <div className="flex items-center">
-              <IconCircleCheck className="size-10 mr-2 text-secondary" />
-              <p className="text-lg text-black font-semibold">
-                Eco-friendly cleaning products
-              </p>
-            </div>
-          </div>
-          <Link
-            href="/book-appointment"
-            className={cn(buttonVariants({ variant: "default" }))}
+      <div className="px-16 py-12 w-full flex flex-col md:flex-row justify-center items-center  bg-secondary text-white">
+        <div className="md:max-w-[80rem] flex flex-col lg:flex-row justify-between  w-full">
+          <CommitmentCard
+            title="100% Guarantee"
+            description="We stand by our work with a 100% guarantee, ensuring that every clean meets your highest expectations."
           >
-            Book an appointment now!
-          </Link>
+            <IconCheck className="size-16" />
+          </CommitmentCard>
+          <CommitmentCard
+            title="Best In Class Service"
+            description="Our commitment to best-in-class service means we deliver meticulous, top-tier cleaning with a personal touch."
+          >
+            <IconRibbon className="size-16" />
+          </CommitmentCard>
+          <CommitmentCard
+            title="Competitive Pricing"
+            description="Competitive pricing that ensures you receive outstanding value and quality, making a pristine home accessible to everyone."
+          >
+            <IconCheck className="size-16" />
+          </CommitmentCard>
         </div>
-
-        <Image
-          src={femaleCleaner}
-          height="750"
-          width="500"
-          alt="hello"
-          className="bg-background"
-        />
       </div>
+      <div className="px-16 py-10 lg:py-20 w-full items-center bg-white flex flex-col lg:flex-row justify-center">
+        <div className="md:max-w-[80rem] flex flex-col lg:flex-row items-center">
+          <div className="flex flex-col items-center lg:items-start text-center lg:text-start space-y-4 ">
+            <h2 className="text-3xl lg:text-5xl text-black font-bold">
+              Commited to quality and customer satisfaction
+            </h2>
+            <p className="text-sm lg:text-lg text-slate-400">
+              We provide a range of services to meet each of your house cleaning
+              needs. Whatever problem you&apos;re facing, we have a solution!
+            </p>
+            <div className="flex flex-col items-start space-y-4">
+              <div className="flex items-center">
+                <IconCircleCheck className="size-10 mr-2 text-secondary" />
+                <p className="text-lg text-black font-semibold">
+                  100% Satisfaction Guarantee
+                </p>
+              </div>
+              <div className="flex items-center">
+                <IconCircleCheck className="size-10 mr-2 text-secondary" />
+                <p className="text-lg text-black font-semibold">
+                  Highly professional staff
+                </p>
+              </div>
+              <div className="flex items-center">
+                <IconCircleCheck className="size-10 mr-2 text-secondary" />
+                <p className="text-lg text-black font-semibold">
+                  Eco-friendly cleaning products
+                </p>
+              </div>
+            </div>
+            <Link
+              href="/book-appointment"
+              className={cn(buttonVariants({ variant: "default" }))}
+            >
+              Book an appointment now!
+            </Link>
+          </div>
+
+          <Image
+            src={femaleCleaner}
+            height="750"
+            width="500"
+            alt="hello"
+            className="bg-background"
+          />
+        </div>
+      </div>
+
       <div className="py-12 px-5 w-full flex flex-col justify-between items-center space-y-8 text-center bg-white">
         <h2 className="text-3xl text-black font-bold">Services Provided</h2>
-        <div className="mx-auto grid justify-center gap-12 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
+        <div className="mx-auto grid justify-center gap-12 sm:grid-cols-2 md:max-w-[80rem] md:grid-cols-3">
           <ServiceCard
             title="Standard Cleaning"
             description="Our standard cleaning package includes dusting, vacuuming, sweeping, and mopping of common areas, plus a thorough cleaning and sanitizing of your kitchen and bathrooms. Keep your home fresh and tidy with our regular maintenance service!"
@@ -146,6 +153,86 @@ export default function Home() {
           </ServiceCard>
         </div>
       </div>
+
+      {/* FAQ SECTION - Start */}
+      <div className="px-8 py-12 w-full bg-white">
+        <section
+          id="faq"
+          className="bg-white"
+          data-aos="fade-right"
+          data-aos-duration="1000"
+        >
+          <div className="max-w-md px-6 mx-auto pb-28 sm:max-w-3xl lg:max-w-7xl lg:px-8">
+            <div className="flex flex-col justify-center text-center">
+              <p className="text-lg font-semibold text-custom">FAQ's</p>
+              <h2 className="mt-2 text-4xl font-bold tracking-tight text-black">
+                Frequently Asked Questions
+              </h2>
+              <p className="pb-10 mx-auto mt-5 text-xl text-gray-900 max-w-prose">
+                Answering your questions about house cleaning in Scottsdale.
+              </p>
+            </div>
+            <div className="max-w-4xl mx-auto">
+              <dl className="mt-10 space-y-6 divide-y divide-gray-900/20">
+                {[
+                  {
+                    question:
+                      "What does a standard house cleaning service in Scottsdale, AZ include?",
+                    answer:
+                      "A standard house cleaning in Scottsdale, AZ typically includes dusting, sweeping, vacuuming, mopping, sanitizing kitchen and bathroom surfaces, and cleaning common areas to keep your home fresh and well-maintained.",
+                  },
+                  {
+                    question:
+                      "Why hire professional house cleaning services in Scottsdale, AZ?",
+                    answer:
+                      "Hiring professional house cleaning services in Scottsdale, AZ ensures a healthier home environment, saves time, and guarantees thorough cleaning using proper tools and products tailored to your specific needs.",
+                  },
+                  {
+                    question:
+                      "How do I choose the #1 house cleaning service in Scottsdale, AZ?",
+                    answer:
+                      "To find the #1 house cleaning service in Scottsdale, AZ, look for proven experience, client reviews, flexible scheduling, eco-friendly options, and a satisfaction guarantee for reliable and high-quality results.",
+                  },
+                ].map((faq, idx) => (
+                  <div className="pt-6" key={idx} data-frequent-question>
+                    <dt>
+                      <button
+                        type="button"
+                        className="flex items-start justify-between w-full text-left text-black"
+                        onClick={(e) => {
+                          const dd =
+                            e.currentTarget.parentElement!.nextElementSibling!;
+                          dd.classList.toggle("hidden");
+                        }}
+                      >
+                        <span className="text-base font-semibold leading-7">
+                          {faq.question}
+                        </span>
+                        <span className="flex items-center ml-6 h-7">
+                          <Image
+                            src="/dropdown-icon.svg"
+                            className="w-6"
+                            alt="Dropdown Icon."
+                            width={24}
+                            height={24}
+                          />
+                        </span>
+                      </button>
+                    </dt>
+                    <dd className="hidden pr-12 mt-2">
+                      <p className="text-base leading-7 text-gray-700">
+                        {faq.answer}
+                      </p>
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      {/* FAQ SECTION - End */}
     </main>
   );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -12,38 +12,6 @@ import {
 export default function Footer() {
   return (
     <footer className="w-full px-14 py-14 flex flex-col md:flex-row justify-between items-center bg-background text-primary">
-      <div className="flex flex-col items-center text-center space-y-8">
-        <div>
-          <p className="font-bold underline">Contact Us</p>
-          <p>(623) 295-0506</p>
-          <a href="mailto:hello@sunvalleycleaners.com" className="underline">
-            hello@sunvalleycleaners.com
-          </a>
-        </div>
-
-        <div>
-          <p className="font-bold underline">Hours of Operation</p>
-          <p>Sunday - Saturday 8am - 8pm</p>
-        </div>
-
-        {/* <Link
-          href="https://www.facebook.com/profile.php?id=61559979211837"
-          target="_blank"
-          className="rounded-md p-1 hover:bg-primary/90"
-        >
-          <IconFacebook className="size-5 text-background" />
-        </Link>
-        <Link href="" className="rounded-md p-1 hover:bg-primary/90">
-          <IconInstagram className="size-5 text-background" />
-        </Link>
-        <Link
-          href="https://www.facebook.com/profile.php?id=61559979211837"
-          target="_blank"
-          className="rounded-md p-1 hover:bg-primary/90"
-        >
-          <IconGoogle className="size-5 text-background" />
-        </Link> */}
-      </div>
       <Image src={logo} height="250" width="250" alt="hello" />
       <div className="my-8 flex flex-col items-start">
         <p className="font-bold underline">Services Provided</p>
@@ -82,6 +50,50 @@ export default function Footer() {
           <IconEvent className="mr-2 size-4" />
           Event Cleaning
         </Link>
+      </div>
+      <div className="my-8 flex flex-row items-center gap-8">
+        <div className="my-8 flex flex-col items-center text-center space-y-8">
+          <div>
+            <p className="font-bold underline">Contact Us</p>
+            <p>(623) 295-0506</p>
+            <a href="mailto:hello@sunvalleycleaners.com" className="underline">
+              hello@sunvalleycleaners.com
+            </a>
+          </div>
+
+          <div>
+            <p className="font-bold underline">Hours of Operation</p>
+            <p>Sunday - Saturday 8am - 8pm</p>
+          </div>
+
+          {/* <Link
+          href="https://www.facebook.com/profile.php?id=61559979211837"
+          target="_blank"
+          className="rounded-md p-1 hover:bg-primary/90"
+        >
+          <IconFacebook className="size-5 text-background" />
+        </Link>
+        <Link href="" className="rounded-md p-1 hover:bg-primary/90">
+          <IconInstagram className="size-5 text-background" />
+        </Link>
+        <Link
+          href="https://www.facebook.com/profile.php?id=61559979211837"
+          target="_blank"
+          className="rounded-md p-1 hover:bg-primary/90"
+        >
+          <IconGoogle className="size-5 text-background" />
+        </Link> */}
+        </div>
+        <div className="my-8 flex ">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d426064.1028127747!2d-112.3192225!3d33.4590543!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2ebef90fa543f995%3A0xb1810327e8cc3751!2sSun%20Valley%20Cleaners!5e0!3m2!1sen!2sin!4v1752485922160!5m2!1sen!2sin"
+            width="400"
+            height="250"
+            style={{ border: 0 }}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
     </footer>
   );
