aed09a637299607947eedf595b1c7409d6a2e7ef
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2026-08-19 22:25:02 -0700

    redirect scottsdale location page to home page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/locations/scottsdale-az',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  }
+};
 
 export default nextConfig;
