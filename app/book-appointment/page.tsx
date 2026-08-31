b5e353804858f2e45157c4fd028083035964aa70
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2026-08-30 23:04:39 -0700

    removed text at top of booking page

--- app/book-appointment/page.tsx
+++ app/book-appointment/page.tsx
@@ -13,25 +13,6 @@ export const metadata = {
 export default function BookAppointmentPage() {
   return (
     <div className="container min-h-screen w-full flex flex-col items-center">
-      <div className="h-[400px] w-full max-w-[600px] flex flex-col md:flex-row justify-center items-center space-y-4 text-center">
-        <h1 className="text-4xl text-foreground font-bold">
-          Book a cleaning with us today!
-        </h1>
-        <p className="text-muted-foreground">
-          Book a cleaning by completing the form below to the best of your
-          ability! If you&apos;d rather secure a cleaning with a phone call,
-          please call (623) 295-0506 and we&apos;ll get you setup!
-        </p>
-        <p className="text-xs text-muted-foreground">
-          If you cannot access the form below for whatever reason, please{" "}
-          <Link
-            href="https://sunvalleycleaners.bookingkoala.com/booknow"
-            className="underline"
-          >
-            book your appointment on our provider&apos;s website
-          </Link>.
-        </p>
-      </div>
       <iframe
         src="https://sunvalleycleaners.bookingkoala.com/booknow/home_cleaning?embed=true&bar=false&banner=false"
         style={{ border: "none", height: "1000px" }}
